b0e259963bd797809dcbe28cfe66832d54e2fa06
sshinichi-69 <77840594+sshinichi-69@users.noreply.github.com>
2021-11-03 06:03:06 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Group from './components/pages/group/Group';
+import CreateGroup from './components/pages/CreateGroup/CreateGroup';
+
+function App() {
+  return (
+    <div>
+      <CreateGroup/>
+    </div>
+  );
+}
+
+export default App;
